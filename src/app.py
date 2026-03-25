665e8e73a67f4871fd86aafc55f4472f6e32d86d
Andres Mena <andresmena284@gmail.com>
2026-03-25 00:01:15 -0500

    avanze del app y servicios

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from servicios import agregar_producto , mostrar_inventario , buscar_producto
+from servicios import agregar_producto , mostrar_inventario , buscar_producto , actualizar_producto , eliminar_producto , calcular_estadisticas
 
 inventario = []
 
@@ -7,12 +7,14 @@ precio_del_producto = float(input("Por favor escribe el precio del producto: "))
 Cantidad_del_producto = int(input("Por favor escribe la cantidad del producto: "))
 
 agregar_producto(inventario,nombre_del_producto,precio_del_producto,Cantidad_del_producto)
+
 print("Producto agregado correctamente.\n")
 
 
 mostrar_inventario(inventario)
 
 buscar_nombre = input("Ingresa el producto que deseas buscar: ")
+
 producto = buscar_producto(inventario,buscar_nombre)
 
 if producto:
@@ -20,3 +22,36 @@ if producto:
     print(producto)
 else:
     print("No se encontro el producto")
+
+nombre = input("Por favor escribe el producto al cual deseas actualizar: ")
+nuevo_precio = float(input("Nuevo precio: "))
+nueva_cantidad = int(input("Nueva cantidad: "))
+
+actualizado = actualizar_producto(inventario,nombre,nuevo_precio,nueva_cantidad)
+
+if actualizado:
+    print("Se actualizo el producto correctamente")
+else:
+    print("No se encontro el producto")
+
+mostrar_inventario(inventario)
+
+nombre = input("Por escribe el producto que deseas eliminar: ")
+
+eliminado = eliminar_producto(inventario,nombre)
+
+if eliminado:
+    print("Se ha eliminado el producto")
+else:
+    print("No se ha encontrado el producto")
+
+
+estadisticas = calcular_estadisticas(inventario)
+
+if estadisticas:
+    print(f"Unidades totales: {estadisticas['unidades_totales']}")
+    print(f"Volar total: {estadisticas['valor_total']}")
+    print(f"Producto mas caro: {estadisticas['producto_mas_caro']['nombre']} - {estadisticas['producto_mas_caro']['precio']}")
+    print(f"Producto mayor stock: {estadisticas['producto_mayor_stock']['nombre']} - {estadisticas['producto_mayor_stock']['cantidad']}")
+else:
+    print("Inventario vacio")
--- src/servicios.py
+++ src/servicios.py
@@ -21,3 +21,54 @@ def buscar_producto(inventario,nombre):
         if producto["nombre"].lower() == nombre.lower():
             return producto
     return None
+
+
+def actualizar_producto(inventario,nombre,nuevo_precio=None,nueva_cantidad=None):
+    
+    for producto in inventario:
+        if producto ["nombre"].lower() == nombre.lower():
+            
+            if nuevo_precio is not None:
+                producto["precio"] = nuevo_precio
+            if nueva_cantidad is not None:
+                producto["cantidad"] = nueva_cantidad
+            return True
+        
+    return False
+
+
+def eliminar_producto (inventario,nombre):
+    for producto in inventario:
+        if producto ["nombre"].lower() == nombre.lower():
+            inventario.remove(producto)
+            return True
+        
+    return False
+
+def calcular_estadisticas(inventario):
+    if len(inventario) == 0:
+        return None
+    
+    unidades_totales = 0
+    valor_total = 0 
+    
+    producto_mas_caro = inventario[0]
+    producto_mayor_stock = inventario[0]
+    
+    for producto in inventario:
+        
+        unidades_totales += producto["cantidad"]
+        valor_total += producto["precio"]*producto["cantidad"]
+        
+        if producto["precio"] > producto_mas_caro["precio"]:
+            producto_mas_caro = producto
+        
+        if producto["cantidad"] > producto_mayor_stock["cantidad"]:
+            producto_mayor_stock = producto
+    
+    return {
+        "unidades_totales": unidades_totales,
+        "valor_total": valor_total,
+        "producto_mas_caro": producto_mas_caro,
+        "producto_mayor_stock": producto_mayor_stock
+    }
